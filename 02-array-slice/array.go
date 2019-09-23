5832baf84e61822ec26a4c7f15ce4962e2569fc3
bens <gleamy_ming@126.com>
2019-09-23 01:41:57 -0700

    array & slice

--- 02-array-slice/array.go
+++ 02-array-slice/array.go
@@ -5,35 +5,39 @@ import "fmt"
 func main() {
 
 	// - 数组的声明
-	// var arr1 [5]int
-	arr1 := [...]int {1,2,3};
-	//   - 值传递
-	for i, v := range arr1 {
-		arr1[i] = i + 1
-		fmt.Printf("arr1 %d, %d\n", i, v)
+	// var a1 [5]int
+	a1 := [...]int{1, 2, 3}
+	for i, v := range a1 {
+		fmt.Printf("a1 %d, %d\n", i, v)
 	}
-	for i, v := range arr1 {
-		fmt.Printf("arr1 %d, %d\n", i, v)
-	}
-	// PrintArray("array:", arr1)
+	// PrintSlice("a1:", a1)
 	//   - 数组不允许扩展
-	// arr1 = append(arr1, 5)
-
+	// a1 = append(a1, 5)
 
 	// - 切片
-	arr2 := []int{9, 8, 7, 6}
-	PrintArray("slice: ", arr2)
-	//   - 可以扩展 
-	arr2 = append(arr2, 5)
+	s1 := []int{1, 2, 3, 4}
+	PrintSlice("s1-0: ", s1)
+	//   - 可以扩展
+	s1 = append(s1, 5)
+	PrintSlice("s1-1: ", s1)
 
+	s2 := s1[2:3]
+	PrintSlice("s2-0 s2 := s1[2:3] : ", s2)
+
+	s2 = append(s2, -1);
+	PrintSlice("s2-1 s2 = append(s2, -1) : ", s2)
+	PrintSlice("s1-2", s1)
+
+	s3 := s1[:2]
+	PrintSlice("s3-0 s3 := s1[:2] : ", s3)
 }
 
-func PrintArray(lab string, arr []int) {
-	for i := 0; i < len(arr); i++ {
-		fmt.Printf("%s %d, %d\n", lab, i, arr[i])
+func PrintSlice(lab string, s []int) {
+	// for i := 0; i < len(arr); i++ {
+	// 	fmt.Printf("%s %d, %d\n", lab, i, arr[i])
+	// }
+	fmt.Printf("%s len=%d cap=%d slice=%v\n", lab, len(s), cap(s), s)
+	for i, v := range s {
+		fmt.Printf("%s %d, %d\n", lab, i, v)
 	}
-
-	//for i, v := range arr {
-        //        fmt.Printf("%s %d, %d\n", lab, i, v)
-        //}
 }

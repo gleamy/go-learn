30a2d875c2960eeb8cb25341649481d5be7bf174
bens <gleamy_ming@126.com>
2019-09-24 21:00:12 -0700

    channel

--- 03-goroutine/channel_range_close.go
+++ 03-goroutine/channel_range_close.go
@@ -1,14 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 func fibonacci(n int, c chan int) {
 	x, y := 1, 1
 	for i := 0; i < n; i++ {
 		c <- x
+		runtime.Gosched()
 		fmt.Println("put in channel %d", x)
 		x, y = y , x + y
-		
 	}
 	close(c)
 }
@@ -16,7 +19,15 @@ func fibonacci(n int, c chan int) {
 func main() {
 	c := make(chan int, 10)
 	go fibonacci(cap(c), c)
-	for i := range c {
-		fmt.Println(i)
+	//for i := range c {
+	//	fmt.Println(i)
+	//}
+	for ; true; {
+		v, ok := <-c
+		if (!ok) {
+			break
+		}
+		fmt.Println(v)
 	}
+
 }
